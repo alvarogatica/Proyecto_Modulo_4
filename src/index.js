073d6ed6924eaa2a452d2901aff6fb78a4a9b037
alvarogatica <alvaroandresgaticariquelme@gmail.com>
2025-04-14 17:23:58 -0400

    agregando manejo de errores try catch

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-const express = require('express');
-const { healthcheck } = require('./controllers/healthcheck.controller');
-const { crearReserva, obtenerTodasLasReservas } = require('./controllers/reservas.controller');
-const reservasRouter = require('./routes/reservas.routes');
-require ('dotenv').config();
+const express = require("express");
+const { healthcheck } = require("./controllers/healthcheck.controller");
+const {
+  crearReserva,
+  obtenerTodasLasReservas,
+} = require("./controllers/reservas.controller");
+const reservasRouter = require("./routes/reservas.routes");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 
-app.get('/', healthcheck);
+app.get("/", healthcheck);
 
-app.use('/reserva', reservasRouter);
-
+app.use("/reserva", reservasRouter);
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Escuchando reservas en el puerto ${port}`);
-})
+  console.log(`Escuchando reservas en el puerto ${port}`);
+});
